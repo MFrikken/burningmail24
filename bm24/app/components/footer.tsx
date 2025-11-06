eeeaeff0770583898bdc7e8048caadee508d8561
mfrikken <frinken.milan@gmail.com>
2025-11-06 20:45:16 +0800

    Added separator and disclaimer

--- bm24/app/components/footer.tsx
+++ bm24/app/components/footer.tsx
@@ -3,10 +3,20 @@ import "../css/footer.css";
 
 export default function Footer() {
     return (
-      <div className="footer">
-          <span>About</span>
-          <span>License</span>
-          <span>Buy us a coffee</span>
-      </div>
+        <div>
+            <span className={"disclaimer"}>This site collects no personal data and uses no cookies. </span>
+            <hr/>
+            <div className="footer">
+                <a href="https://burningmail24.live/license" target="_blank" rel="noopener noreferrer">
+                    ©2025 burningmail24.live
+                </a>
+                <a href="https://github.com/MFrikken/burningmail24" target="_blank" rel="noopener noreferrer">
+                    View Source
+                </a>
+                <a href="https://paypal.me/frikken" target="_blank" rel="noopener noreferrer">
+                    Support us
+                </a>
+            </div>
+        </div>
     );
 }
