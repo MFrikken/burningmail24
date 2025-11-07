983c9d49f34a95fbb4122aa18142a2847dc43fa9
mfrikken <frinken.milan@gmail.com>
2025-11-07 14:57:23 +0800

    Updated footer

--- bm24/app/components/footer.tsx
+++ bm24/app/components/footer.tsx
@@ -4,17 +4,11 @@ import "../css/footer.css";
 export default function Footer() {
     return (
         <div>
-            <span className={"disclaimer"}>This site collects no personal data and uses no cookies. </span>
             <hr/>
             <div className="footer">
-                <a href="https://burningmail24.live/license" target="_blank" rel="noopener noreferrer">
-                    ©2025 burningmail24.live
-                </a>
+                <span className={"disclaimer"}>This site collects no personal data and uses no cookies. </span>
                 <a href="https://github.com/MFrikken/burningmail24" target="_blank" rel="noopener noreferrer">
-                    View Source
-                </a>
-                <a href="https://paypal.me/frikken" target="_blank" rel="noopener noreferrer">
-                    Support us
+                    ©2025 burningmail24.live | Licensed under MIT License
                 </a>
             </div>
         </div>
