ca547ecdfaa3e5e985f22b99932f6b36b5eb2ddf
mfrikken <frinken.milan@gmail.com>
2025-11-10 20:07:03 +0800

    Fixed misalignment of text area for long subject lines

--- bm24/app/components/input-email-body.tsx
+++ bm24/app/components/input-email-body.tsx
@@ -24,24 +24,26 @@ export default function InputEmailBody({sampleMail, fetchRequest}: InputEmailBod
 
     return (
         <div className={"input-email-body"}>
-            <h3 className={"input-header"}>Input your email body here</h3>
-            <textarea
-                className={"input-text-area"}
-                placeholder={sampleMail}
-                maxLength={500}
-                autoFocus={true}
-                cols={80}
-                rows={20}
-                required={true}
-                onChange={(e) => setMailbody(e.target.value)}
-            />
-            <button
-                className={"submit-button"}
-                type={"submit"}
-                onClick={submit}
-            >
-                Generate
-            </button>
+            <div className={"wrapper"}>
+                <h3 className={"input-header"}>Input your email body here</h3>
+                <textarea
+                    className={"input-text-area"}
+                    placeholder={sampleMail}
+                    maxLength={500}
+                    autoFocus={true}
+                    cols={80}
+                    rows={20}
+                    required={true}
+                    onChange={(e) => setMailbody(e.target.value)}
+                />
+                <button
+                    className={"submit-button"}
+                    type={"submit"}
+                    onClick={submit}
+                >
+                    Generate
+                </button>
+            </div>
         </div>
     );
 }
