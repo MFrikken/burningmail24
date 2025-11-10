82cead7139ea83e60b0ae3cf12410fb96cf39c77
mfrikken <frinken.milan@gmail.com>
2025-11-10 16:52:53 +0800

    Changed favicon and removed unused images

--- bm24/app/page.tsx
+++ bm24/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import InputEmailBody from "@/app/components/input-email-body";
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 import Body from "@/app/components/body";
