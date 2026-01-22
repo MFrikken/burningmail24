1a6cb05497d9ca88865544f3cf4acd479540ccec
mfrikken <frinken.milan@gmail.com>
2026-01-22 17:20:48 +0800

    Optimized logo and changed <Logo/> to primitive <img/>

--- bm24/app/components/header.tsx
+++ bm24/app/components/header.tsx
@@ -1,11 +1,10 @@
 import "../css/header.css";
-import Image from "next/image";
 
 export default function Header() {
     return (
         <div className={"header"}>
             <h1>burningmail24</h1>
-            <Image src="/logo.svg" alt="burningmail24 logo" width={64} height={64} />
+            <img src="/logo.svg" alt="Burningmail24 Logo" width={64} height={64} decoding="async" />
         </div>
     );
 }
